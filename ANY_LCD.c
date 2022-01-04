59d34fc8760863ede7dfd73878ef632fa064e615
Marquez Jones <34190119+Marquez607@users.noreply.github.com>
2022-01-04 14:20:18 -0500

    Update ANY_LCD.c

--- ANY_LCD.c
+++ ANY_LCD.c
@@ -186,7 +186,7 @@ static void LCD_SendBytes(uint16_t *buffer,any_lcd_t lcd,uint8_t len){
 	for(uint8_t i = 0;i < len;i++){
 		
 		lcd.lcd_write(buffer[i]);
-        lcd.delay_us(5E3);
+        lcd.delay_us(1E3);
 		
 	}
 
